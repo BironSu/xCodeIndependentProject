1f39aaf7f7a206771804c82b0057cfbb00f6b3a2
BironSu <bironsu@pursuit.org>
2018-10-05 15:10:00 -0400

    Initial Commit

---
+++ xCodeNewProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xCodeNewProject
+//
+//  Created by Biron Su on 10/5/18.
+//  Copyright © 2018 Biron Su. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
